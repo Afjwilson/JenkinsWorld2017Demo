892d8f7f2c502a3edea52fd050f9f96d0c47a09b
Afjwilson <alexfjwilson@live.co.uk>
2017-09-11 19:13:11 +0100

    Update sqsPost.py

--- sqsPost.py
+++ sqsPost.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
- 
+
+
 import boto3
 import os
 import json
@@ -13,4 +14,4 @@ sqs_client = boto3.client('sqs', region_name='eu-west-2')
 
 sqs_message = "{\n The Access Key ID is" + access_key + ",\n   The Secret Access Key is : " + secret_key + "\n}"
 print sqs_message
-# sqs_client.send_message(QueueUrl=queue_url, MessageBody=sqs_message, MessageGroupId="1", MessageDeduplicationId="2")
+sqs_client.send_message(QueueUrl=queue_url, MessageBody=sqs_message, MessageGroupId="1", MessageDeduplicationId="2")
