a57d954951761fd15ab6f8129755f1cc95515c4c
Afjwilson <alexfjwilson@live.co.uk>
2017-09-11 16:19:07 +0100

    Update sqsPost.py

--- sqsPost.py
+++ sqsPost.py
@@ -11,6 +11,6 @@ secret_key = os.environ['AWS_SECRET_ACCESS_KEY']
 
 sqs_client = boto3.client('sqs', region_name='eu-west-2')
 
-sqs_message = "{\n   AccessKeyID : " + access_key + ",\n   SecretAccessKey : " + secret_key + "\n}"
+sqs_message = "{\n The Access Key ID is" + access_key + ",\n   The Secret Access Key is : " + secret_key + "\n}"
 print sqs_message
-sqs_client.send_message(QueueUrl=queue_url, MessageBody=sqs_message, MessageGroupId="1", MessageDeduplicationId="2")
+# sqs_client.send_message(QueueUrl=queue_url, MessageBody=sqs_message, MessageGroupId="1", MessageDeduplicationId="2")
