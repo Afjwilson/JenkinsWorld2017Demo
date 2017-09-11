feedc880a8b1b603de9121566f223dbd7a175073
Afjwilson <alexfjwilson@live.co.uk>
2017-09-11 13:01:00 +0100

    Update sqsPost.py

--- sqsPost.py
+++ sqsPost.py
@@ -4,12 +4,12 @@ import boto3
 import os
 import json
 
-queue_url = "https://sqs.us-east-1.amazonaws.com/085857639847/JenkinsWorldDemo.fifo" 
+queue_url = "https://sqs.eu-west-2.amazonaws.com/860708036433/JenkinsWorldDemo" 
 
 access_key = os.environ['AWS_ACCESS_KEY_ID']
 secret_key = os.environ['AWS_SECRET_ACCESS_KEY']
 
-sqs_client = boto3.client('sqs', region_name='us-east-1')
+sqs_client = boto3.client('sqs', region_name='eu-west-2')
 
 sqs_message = "{\n   AccessKeyID : " + access_key + ",\n   SecretAccessKey : " + secret_key + "\n}"
 print sqs_message
